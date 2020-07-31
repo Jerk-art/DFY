8520e6721dafdda1eb4f9570c2eef56b35cd16a0
jerk <jerkinston@gmail.com>
2020-07-31 14:35:31 +0300

    v0.0.2

--- dfy.py
+++ dfy.py
@@ -1,5 +1,9 @@
-from app import app, db
+from app import create_app, db
 from app.models import Task
+
+app = create_app()
+app.app_context().push()
+Task.stop_all_tasks()
 
 
 @app.shell_context_processor
