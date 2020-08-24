5164268d24dbac3ff1899fd62cf0560803c80151
jerk <jerkinston@gmail.com>
2020-08-24 11:48:20 +0300

    v0.0.4

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,25 +1,27 @@
 from flask_wtf import FlaskForm
 from flask import current_app
 from wtforms import SubmitField
+from wtforms import SelectField
+from wtforms import IntegerField
 from wtforms.fields.html5 import URLField
 from wtforms.validators import DataRequired, url
 from wtforms.validators import ValidationError
 from app.tasks import get_yt_file_info
 from app.tasks import get_sc_file_info
 from app.tasks import is_allowed_duration
+from app.tasks import get_yt_playlist_info
 from app.tasks import BadUrlError
 
 
+QUALITY_LIST = [(1, 64), (2, 128), (3, 192), (4, 320)]
+
+
+def get_quality(num):
+    return QUALITY_LIST[num - 1][1]
+
+
 def content_check(form, field):
-    if field.data.startswith('https://www.youtube.com/watch?v='):
-        try:
-            info = get_yt_file_info(field.data)
-        except BadUrlError:
-            raise ValidationError(f'Please check the link for correctness.')
-        if not is_allowed_duration(info):
-            raise ValidationError(f'Video/music file must be shorter '
-                                  f'than {current_app.config["ALLOWED_DURATION"]} minutes.')
-    elif field.data.startswith('https://youtu.be/'):
+    if field.data.startswith('https://www.youtube.com/watch?v=') or field.data.startswith('https://youtu.be/'):
         try:
             info = get_yt_file_info(field.data)
         except BadUrlError:
@@ -35,8 +37,62 @@ def content_check(form, field):
         if not is_allowed_duration(info):
             raise ValidationError(f'Video/music file must be shorter '
                                   f'than {current_app.config["ALLOWED_DURATION"]} minutes.')
+    else:
+        raise ValidationError('Please check link for correctness.')
 
 
 class DownloadForm(FlaskForm):
     link = URLField(validators=[DataRequired(), url(), content_check])
     submit = SubmitField('Download')
+
+
+class DownloadForm2(FlaskForm):
+    link = URLField(validators=[DataRequired(), url(), content_check])
+    quality = SelectField('Quality',
+                          choices=QUALITY_LIST,
+                          default=3,
+                          validators=[DataRequired()],
+                          coerce=int)
+    submit = SubmitField('Download')
+
+
+class DownloadPlaylistItemsForm(FlaskForm):
+    link = URLField('Link to playlist', validators=[DataRequired(message='Please enter URL.'), url()])
+    first_item_number = IntegerField('First item -->',
+                                     validators=[DataRequired(message='Please enter both items numbers.')])
+    last_item_number = IntegerField('<-- Last item',
+                                    validators=[DataRequired(message='Please enter both items numbers.')])
+    quality = SelectField('Quality',
+                          choices=QUALITY_LIST,
+                          default=3,
+                          validators=[DataRequired()],
+                          coerce=int)
+    submit = SubmitField('Download')
+
+    def validate_link(self, link):
+        if link.data.startswith('https://www.youtube.com/watch?v='):
+            pass
+        elif link.data.startswith('https://www.youtube.com/playlist?list=') and len(link.data) > 71:
+            pass
+        else:
+            print(len(link.data))
+            raise ValidationError('Please check link for correctness.')
+
+    @staticmethod
+    def validate_first_item_number(form, first_item_number):
+        if first_item_number.data < 1:
+            raise ValidationError('Bad first item number.')
+        if form.link.data.startswith('https://www.youtube.com/playlist?list='):
+            pid = form.link.data[38:]
+        else:
+            index = form.link.data.find('&index=')
+            pid = form.link.data[49:index]
+        try:
+            if first_item_number.data > get_yt_playlist_info(pid)['pageInfo']['totalResults']:
+                raise ValidationError('Bad first item number.')
+        except BadUrlError:
+            raise ValidationError('Please check passed link for correctness.')
+
+    def validate_last_item_number(self, last_item_number):
+        if last_item_number.data < 2:
+            raise ValidationError('Bad first item number.')
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,23 +1,35 @@
 from app import db
 from app.tasks import download
+from app.tasks import download_yt_files
 from app.tasks import get_download_response
+from app.tasks import get_yt_playlist_items
 from app.main import bp
 from app.main.forms import DownloadForm
+from app.main.forms import DownloadForm2
+from app.main.forms import DownloadPlaylistItemsForm
+from app.main.forms import get_quality
 from app.models import Task
+from flask import abort
 from flask import render_template
 from flask import redirect
 from flask import request
 from flask import flash
 from flask import url_for
 from flask import jsonify
+from flask import current_app
+from flask_login import current_user
 from youtube_dl import DownloadError
 
 
 @bp.route('/', methods=['GET'])
 @bp.route('/index', methods=['GET'])
 def index():
-    youtube_form = DownloadForm()
-    soundcloud_form = DownloadForm()
+    if current_user.is_authenticated:
+        youtube_form = DownloadForm2()
+        soundcloud_form = DownloadForm2()
+    else:
+        youtube_form = DownloadForm()
+        soundcloud_form = DownloadForm()
     active_slide = request.args.get('active_slide', 1, type=int)
     if active_slide == 1:
         return render_template('index.html', title="Young downloader",
@@ -33,59 +45,186 @@ def index():
 def download_yt():
     if request.method == "GET":
         return redirect(url_for('main.index', active_slide=1))
-    youtube_form = DownloadForm()
-    if youtube_form.validate_on_submit():
-        t = Task.query.filter_by(user_ip=str(request.remote_addr), status_code=0).first()
-        if t:
-            flash('Please wait while your other downloading is complete.', 'yt')
+    if not current_user.is_authenticated:
+        youtube_form = DownloadForm()
+        if youtube_form.validate_on_submit():
+            t = Task.query.filter_by(user_ip=str(request.remote_addr), status_code=0).first()
+            if t:
+                flash('Please wait while your other downloading is complete.', 'yt')
+                return redirect(url_for('main.index', active_slide=1))
+            t = Task(description="Downloading mp3", user_ip=str(request.remote_addr), status_code=0, progress='Waiting')
+            quality = 192
+            print(f'Loading from youtube user ip:{request.remote_addr} data:{youtube_form.link.data}')
+        else:
+            flash(youtube_form.link.errors[0], 'yt')
             return redirect(url_for('main.index', active_slide=1))
-        print(f'Loading from youtube ip:{request.remote_addr} data:{youtube_form.link.data}')
-        t = Task(description="Downloading mp3", user_ip=str(request.remote_addr), status_code=0, progress='Waiting')
-        db.session.add(t)
-        db.session.commit()
-        try:
-            if youtube_form.link.data.startswith('https://www.youtube.com/watch?v='):
-                file = download(youtube_form.link.data[:43], t)
-            else:
-                file = download(youtube_form.link.data[:28], t)
-        except DownloadError as err:
-            t.force_stop('Download error')
-            raise err
-        return get_download_response(file, t)
+
     else:
-        flash(youtube_form.link.errors[0], 'yt')
-        return redirect(url_for('main.index', active_slide=1))
+        youtube_form = DownloadForm2()
+        if youtube_form.validate_on_submit():
+            t = Task.query.filter_by(user_id=current_user.id, status_code=0).first()
+            if t:
+                flash('Please wait while your other downloading is complete.', 'yt')
+                return redirect(url_for('main.index', active_slide=1))
+            t = Task(description="Downloading mp3", user_id=current_user.id, status_code=0, progress='Waiting')
+            quality = get_quality(youtube_form.quality.data)
+            print(f'Loading from youtube user id:{current_user.id} data:{youtube_form.link.data}')
+        else:
+            flash(youtube_form.link.errors[0], 'yt')
+            return redirect(url_for('main.index', active_slide=1))
+
+    db.session.add(t)
+    db.session.commit()
+    try:
+        if youtube_form.link.data.startswith('https://www.youtube.com/watch?v='):
+            file = download(youtube_form.link.data[:43], task=t, quality=quality)
+        else:
+            file = download(youtube_form.link.data[:28], task=t, quality=quality)
+    except DownloadError as err:
+        t.force_stop('Download error')
+        raise err
+    return get_download_response(file, t)
 
 
 @bp.route('/download_sc', methods=['GET', 'POST'])
 def download_sc():
     if request.method == "GET":
         return redirect(url_for('main.index', active_slide=2))
-    soundcloud_form = DownloadForm()
-    if soundcloud_form.validate_on_submit():
-        t = Task.query.filter_by(user_ip=str(request.remote_addr), status_code=0).first()
-        if t:
-            flash('Please wait while your other downloading is complete.', 'sc')
+    if not current_user.is_authenticated:
+        soundcloud_form = DownloadForm()
+        if soundcloud_form.validate_on_submit():
+            t = Task.query.filter_by(user_ip=str(request.remote_addr), status_code=0).first()
+            if t:
+                flash('Please wait while your other downloading is complete.', 'yt')
+                return redirect(url_for('main.index', active_slide=1))
+            t = Task(description="Downloading mp3", user_ip=str(request.remote_addr), status_code=0, progress='Waiting')
+            quality = 192
+            print(f'Loading from youtube user ip:{request.remote_addr} data:{soundcloud_form.link.data}')
+        else:
+            flash(soundcloud_form.link.errors[0], 'sc')
             return redirect(url_for('main.index', active_slide=2))
-        print(f'Loading from soundcloud ip:{request.remote_addr} data:{soundcloud_form.link.data}')
-        t = Task(description="Downloading mp3", user_ip=str(request.remote_addr), status_code=0, progress='Waiting')
-        db.session.add(t)
-        db.session.commit()
-        try:
-            file = download(soundcloud_form.link.data, t)
-        except DownloadError as err:
-            t.force_stop('Download error')
-            raise err
-        return get_download_response(file, t)
+
     else:
-        flash(soundcloud_form.link.errors[0], 'sc')
-        return redirect(url_for('main.index', active_slide=2))
+        soundcloud_form = DownloadForm2()
+        if soundcloud_form.validate_on_submit():
+            t = Task.query.filter_by(user_id=current_user.id, status_code=0).first()
+            if t:
+                flash('Please wait while your other downloading is complete.', 'yt')
+                return redirect(url_for('main.index', active_slide=1))
+            t = Task(description="Downloading mp3", user_id=current_user.id, status_code=0, progress='Waiting')
+            quality = get_quality(soundcloud_form.quality.data)
+            print(f'Loading from youtube user id:{current_user.id} data:{soundcloud_form.link.data}')
+        else:
+            flash(soundcloud_form.link.errors[0], 'sc')
+            return redirect(url_for('main.index', active_slide=2))
+
+    db.session.add(t)
+    db.session.commit()
+    try:
+        file = download(soundcloud_form.link.data, task=t, quality=quality)
+    except DownloadError as err:
+        t.force_stop('Download error')
+        raise err
+    return get_download_response(file, t)
+
+
+@bp.route('/download_playlist_items', methods=['GET', 'POST'])
+def download_playlist_items():
+    form = DownloadPlaylistItemsForm()
+    if request.method == "GET":
+        if current_user.is_authenticated:
+            t = Task.query.filter_by(user_id=current_user.id, status_code=3).first()
+            if t and t.description == 'Downloading playlist items':
+                return render_template('/download_playlist_progress.html')
+            else:
+                t = Task.query.filter_by(user_id=current_user.id, status_code=4).first()
+                if t:
+                    return render_template('/download_playlist_progress.html')
+            return render_template('/download_playlist_items.html', form=form)
+        else:
+            return redirect(url_for('main.index'))
+    else:
+        if current_user.is_authenticated:
+            if form.validate_on_submit():
+                t = Task.query.filter_by(user_id=current_user.id, status_code=3).first()
+                if t:
+                    return render_template('/download_playlist_progress.html')
+                else:
+                    t = Task.query.filter_by(user_id=current_user.id, status_code=4).first()
+                    if t:
+                        return render_template('/download_playlist_progress.html')
+
+                t = Task(description="Downloading playlist items",
+                         user_id=current_user.id,
+                         status_code=3,
+                         progress='Waiting')
+                quality = get_quality(form.quality.data)
+                print(f'Loading playlist items from youtube user id:{current_user.id} data:{form.link.data}')
+            else:
+                if form.link.errors:
+                    flash(form.link.errors[0])
+                elif form.first_item_number.errors:
+                    flash(form.first_item_number.errors[0])
+                elif form.last_item_number.errors:
+                    flash(form.last_item_number.errors[0])
+                return render_template('/download_playlist_items.html', form=form)
+        else:
+            return redirect(url_for('main.index'))
+
+    db.session.add(t)
+    db.session.commit()
+
+    files_list = get_yt_playlist_items(form.link.data, form.first_item_number.data - 1, form.last_item_number.data)
+
+    download_yt_files(files_list, task=t, quality=quality)
+
+    return render_template('/download_playlist_progress.html')
+
+
+@bp.route('/get_file', methods=['GET'])
+def get_file():
+    if current_user.is_authenticated:
+        t = Task.query.filter_by(user_id=current_user.id, status_code=4).first()
+        if t:
+            if t.progress.startswith('Files downloaded') and t.description == 'Downloading playlist items':
+                return get_download_response(t.unique_process_info, t, ext='zip')
+                return get_download_response(t.unique_process_info, t, ext='zip', end_task=False)
+            else:
+                return abort(403)
+        else:
+            return redirect(url_for('main.download_playlist_items'))
+    else:
+        return abort(404)
 
 
 @bp.route('/get_progress', methods=['GET'])
 def get_progress():
-    t = Task.query.filter_by(user_ip=str(request.remote_addr), status_code=0).first()
+    if not current_user.is_authenticated:
+        t = Task.query.filter_by(user_ip=str(request.remote_addr), status_code=0).first()
+    else:
+        t = Task.query.filter_by(user_id=str(current_user.id), status_code=0).first()
     if t:
-        return jsonify({'Status_code': 0, 'Progress': t.progress})
+        return jsonify({'Status_code': t.status_code, 'Progress': t.progress})
     else:
         return jsonify({'Status_code': 1, 'Progress': 'Done'})
+
+
+@bp.route('/get_playlist_downloading_progress')
+def get_playlist_downloading_progress():
+    if current_user.is_authenticated:
+        t = Task.query.filter_by(user_id=str(current_user.id), status_code=3).first()
+        if not t:
+            t = Task.query.filter_by(user_id=str(current_user.id), status_code=4).first()
+        if t:
+            statuses = str()
+            files = t.files.all()
+            if files and len(files) > 0:
+                for el in sorted(files, key=files.index):
+                    statuses += str(el.status_code)
+                return jsonify({'Status_code': t.status_code, 'Progress': t.progress, 'Status_codes': statuses})
+            else:
+                return jsonify({'Status_code': t.status_code, 'Progress': t.progress, 'Status_codes': None})
+        else:
+            return jsonify({'Status_code': 1, 'Progress': 'Done'})
+    else:
+        abort(403)
